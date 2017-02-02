61db5d6b63093383d93cfe4667e2ff2120d34090
Ryan Cooper <ryancooper729@gmail.com>
2017-02-01 23:56:00 -0500

    host sanborn tiles on mapbox

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// L.mapbox.accessToken = 'pk.eyJ1IjoibWFwdGFzdGlrIiwiYSI6IjNPMkREV1kifQ.2KGPFZD0QaGfvYzXYotTXQ';
 var handle = document.getElementById('handle'),
     start = false,
     startTop;
@@ -13,13 +12,13 @@ var gBase = new L.Google();
 
 map.addLayer(gBase);
 
-var overlay = L.tileLayer('https://lexhousingstudies.github.io/assets/southill1957/{z}/{x}/{y}.png', {
-      attribution: '<a href="http://libraries.uky.edu/" target="_blank">University of Kentucky Libraries</a>',
-      opacity: 0.75,
-      maxNativeZoom: 19,
-      maxZoom: 19
-    })
-    .addTo(map);
+var overlay = L.tileLayer('https://api.mapbox.com/v4/lexhousingstudies.carxus22/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibGV4aG91c2luZ3N0dWRpZXMiLCJhIjoiY2lqY2NhZ2VjMDAxZHV1bTBwc2hkbHJ4bCJ9.6eF0ROiV6x6wBDhdDOv2RA', {
+        attribution: '<a href="http://libraries.uky.edu/" target="_blank">University of Kentucky Libraries</a>',
+        opacity: 0.75,
+        maxNativeZoom: 19,
+        maxZoom: 19
+      })
+      .addTo(map);
 
 document.onmousemove = function(e) {
     if (!start) return;
