e1425279cc4123e26bcdc009615eefa7e7e51cad
Ryan Cooper <ryancooper729@gmail.com>
2017-03-09 01:07:37 -0500

    Update to jQuery Mobile to allow mobile users to use slider

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-
-var handle = document.getElementById('handle'),
-    start = false,
-    startTop;
-
 var map = L.map('map',{
       minZoom: 16,
       maxZoom: 19
@@ -21,21 +16,13 @@ var overlay = L.tileLayer('https://api.mapbox.com/v4/lexhousingstudies.carxus22/
       })
       .addTo(map);
 
-document.onmousemove = function(e) {
-    if (!start) return;
-    // Adjust control.
-    handle.style.top = Math.max(-5, Math.min(195, startTop + parseInt(e.clientY, 10) - start)) + 'px';
-    // Adjust opacity.
-    overlay.setOpacity(1 - (handle.offsetTop / 200));
-};
+  $("#opacity-control").trigger("create");
+  $("#opacity-slider").on("slidestop", function(event, ui){
+    var opacityNew = event.target.valueAsNumber;
+    ChangeOpacity(opacityNew, overlay);
+  })
 
-handle.onmousedown = function(e) {
-    // Record initial positions.
-    start = parseInt(e.clientY, 10);
-    startTop = handle.offsetTop - 5;
-    return false;
-};
-
-document.onmouseup = function(e) {
-    start = null;
-};
+  function ChangeOpacity(slider, overlay){
+    var opacityValue = slider/100;
+    overlay.setOpacity(opacityValue);
+  }
